4406e8db95831d31553f436e593a70c50ae67930
Jerry Lai <jerrydavid123@hotmail.com>
2021-01-28 19:28:20 +0800

    test over

--- app.js
+++ app.js
@@ -20,12 +20,7 @@ let fetchHead = cron.schedule('0 6 * * *', () => {
 let fetchBoxData = cron.schedule('0,30 00,10,12,14 * * *', () => {
     fetchData();
 }, { timezone: 'Asia/Shanghai' })
-fetchTableHead();
 
-app.get('/', (req, res) => {
-    fetchData();
-    res.render('home')
-})
 fetchHead.start();
 fetchBoxData.start();
 app.use(bodyParser.urlencoded({ extended: true }))
