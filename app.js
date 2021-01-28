3106d032d7fee0ce66620a4eb8c5e8f1135e8d46
Jerry Lai <jerrydavid123@hotmail.com>
2021-01-28 19:21:03 +0800

    add time

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ let fetchHead = cron.schedule('0 6 * * *', () => {
     fetchTableHead();
 }, { timezone: 'Asia/Shanghai' })
 
-let fetchBoxData = cron.schedule('0,30 00,10,14 * * *', () => {
+let fetchBoxData = cron.schedule('0,30 00,10,12,14 * * *', () => {
     fetchData();
 }, { timezone: 'Asia/Shanghai' })
 fetchTableHead();
